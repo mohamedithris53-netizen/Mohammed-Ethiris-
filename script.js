f3ac4fadd19f2445447344f54fe0c8bc074bc8b6
mohamedithris53-netizen <mohamedithris53@gmail.com>
2025-09-15 09:20:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+<body>
+
+    <div class="container">
+
+        <div class="card card-body" style="justify-content: center;">
+
+            <!--Contents <h1 >_______</h1> -->
+
+        </div>
+
+    </div>
+
+</body>
